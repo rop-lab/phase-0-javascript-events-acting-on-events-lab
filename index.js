aeaaa0522aebe811753433c11267a9f46c9dd684
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-15 09:37:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var dodger = document.getElementById('dodger');
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if (left < window.innerWidth - dodger.offsetWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
